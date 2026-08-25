c0bc9f5b5abfe833aed378099481e9b1a4894c8a
Pashani Jaulani <pashanijaulani@gmail.com>
2026-08-25 04:07:35 +0200

    Expose interactive topology discovery from dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,9 @@
 import NocDashboard from "./NocDashboard";
+import TopologyView from "./components/TopologyView";
 
-export default NocDashboard;
+export default function App() {
+  return <>
+    <NocDashboard />
+    <TopologyView />
+  </>;
+}
