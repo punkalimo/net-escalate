d3f646fbeff4afb2a4b991f734e26394ed6d5026
Pashani Jaulani <pashanijaulani@gmail.com>
2026-08-25 09:35:37 +0200

    fix: prevent topology errors from blanking dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import NocDashboard from "./NocDashboard";
 import TopologyView from "./components/TopologyView";
 import TopologyInteraction from "./components/TopologyInteraction";
+import TopologyErrorBoundary from "./components/TopologyErrorBoundary";
 
 export default function App() {
   return <>
     <NocDashboard />
-    <TopologyView />
-    <TopologyInteraction />
+    <TopologyErrorBoundary>
+      <TopologyView />
+      <TopologyInteraction />
+    </TopologyErrorBoundary>
   </>;
 }
