62de3cec6d6596a27b678d4be986244b0d79a252
Pashani Jaulani <pashanijaulani@gmail.com>
2026-08-25 10:01:27 +0200

    feat: expose root cause analysis from app shell

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import NocDashboard from "./NocDashboard";
 import TopologyView from "./components/TopologyView";
 import TopologyErrorBoundary from "./components/TopologyErrorBoundary";
+import RootCauseCenter from "./components/RootCauseCenter";
 
 export default function App() {
   return <>
     <NocDashboard />
+    <RootCauseCenter />
     <TopologyErrorBoundary>
       <TopologyView />
     </TopologyErrorBoundary>
