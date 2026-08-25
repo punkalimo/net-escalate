446a358ae3e84715f5bd7f8716218f51fa97d58b
Pashani Jaulani <pashanijaulani@gmail.com>
2026-08-25 03:06:03 +0200

    Integrate interface health, slow-link and historical monitoring center into frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import InterfaceHealthCenter from './components/InterfaceHealthCenter.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <InterfaceHealthCenter />
   </StrictMode>,
 )
