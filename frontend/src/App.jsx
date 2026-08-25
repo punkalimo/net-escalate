a2967ea6a01bda3a97db3859a5bd470e4af574bf
Pashani Jaulani <pashanijaulani@gmail.com>
2026-08-25 12:14:14 +0200

    fix(phase4): use lean command center

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,13 @@ import NocDashboard from "./NocDashboard";
 import TopologyView from "./components/TopologyView";
 import TopologyErrorBoundary from "./components/TopologyErrorBoundary";
 import RootCauseCenter from "./components/RootCauseCenter";
-import Phase4OperationsCenter from "./components/Phase4OperationsCenter";
+import Phase4CommandCenter from "./components/Phase4CommandCenter";
 
 export default function App() {
   return <>
     <NocDashboard />
     <RootCauseCenter />
-    <TopologyErrorBoundary>
-      <TopologyView />
-    </TopologyErrorBoundary>
-    <Phase4OperationsCenter />
+    <TopologyErrorBoundary><TopologyView /></TopologyErrorBoundary>
+    <Phase4CommandCenter />
   </>;
 }
